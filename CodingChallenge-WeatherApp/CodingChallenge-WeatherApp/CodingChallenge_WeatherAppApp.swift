04c0ae4db57b59d130c1dc1255165f3c44b04dce
Samuel Folledo <samuelfolledo@gmail.com>
2024-10-01 17:11:12 -0700

    add initial xcode project and gitignore file

---
+++ CodingChallenge-WeatherApp/CodingChallenge-WeatherApp/CodingChallenge_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallenge_WeatherAppApp.swift
+//  CodingChallenge-WeatherApp
+//
+//  Created by Samuel Folledo on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallenge_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
